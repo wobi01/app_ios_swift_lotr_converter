097205b18399a01e8ee5e7565182fbb483040a85
Wojciech <wbiskup@edu.cdv.pl>
2024-04-13 21:10:41 +0200

    Initial Commit

---
+++ LOTRconverter/LOTRconverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LOTRconverterApp.swift
+//  LOTRconverter
+//
+//  Created by Wobi on 13/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct LOTRconverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
